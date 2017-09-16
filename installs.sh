9811ab5d074712d909dad4027745361387e9013f
Aurimas Bukauskas <ab697@bath.ac.uk>
2017-09-16 18:15:24 +0200

    add an install script

---
+++ installs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Script to Install:
+# nvm
+# node.js
+# http-server
+
+
+# xcode
+xcode-select --install
+
+# nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.4/install.sh | bash
+
+# node.js
+nvm install node
+
+# http-server
+npm install -global http-server
+
+
+
